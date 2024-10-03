6dd61656dc04eb03f3283fe1b29898f2d284e75b
Martin-Louis Bright <mbright@cpacketnetworks.com>
2024-10-03 14:01:04 -0400

    update the SDK

--- Code/index.js
+++ Code/index.js
@@ -1,5 +1,5 @@
-var aws = require("aws-sdk");
-var ec2 = new aws.EC2();
+var aws = require("@aws-sdk/client-ec2");
+var ec2 = new aws.EC2Client();
 
 exports.handler = async function (event) {
   var params = {
